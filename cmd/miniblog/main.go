784f33fd22e0537a16ed99baa669dbff0e6bc2aa
hyj <hyj@zs-shiruan.com>
2023-11-25 17:22:17 +0800

    feat: pflag

--- cmd/miniblog/main.go
+++ cmd/miniblog/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"net/http"
+
+	"github.com/spf13/pflag"
+)
+
+const helpText = `Usage: main [flags] arg [arg...]
+This is a very simple app framework(do nothing).
+Flags:`
+
+var (
+	addr = pflag.String("addr", ":8088", "The address to listen to.")
+	help = pflag.BoolP("help", "h", false, "show help message")
+
+	usage = func() {
+		fmt.Println(helpText)
+		flag.PrintDefaults()
+	}
+)
 
 func main() {
-	fmt.Sprintln("hello world")
+	pflag.Usage = usage
+	pflag.Parse()
+	if *help {
+		pflag.Usage()
+		return
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("hello world")
+	})
+
+	server := http.Server{Addr: *addr}
+	fmt.Printf(`starting http server at %s`, *addr)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+
 }
