1eca7eb357e63fde56fc88d9edfe62707adea151
huangyul <1103221645@qq.com>
2024-01-15 05:52:23 +0800

    feat: add user store

---
+++ internal/miniblog/store/store.go
@@ -0,0 +1,33 @@
+package store
+
+import (
+	"gorm.io/gorm"
+	"sync"
+)
+
+var (
+	once sync.Once
+	s    *database
+)
+
+var _ IStore = &database{}
+
+type IStore interface {
+	User() UserStore
+}
+
+type database struct {
+	db *gorm.DB
+}
+
+func (d *database) User() UserStore {
+	return newUsers(d.db)
+}
+
+func NewStore(db *gorm.DB) *database {
+	once.Do(func() {
+		s = &database{db: db}
+	})
+
+	return s
+}
