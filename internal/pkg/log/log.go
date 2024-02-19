da908d5ac4e2cb49e42013d6e3a74095fcafe1da
huangyul <1103221645@qq.com>
2024-02-20 06:07:34 +0800

    feat: custom logger

--- internal/pkg/log/log.go
+++ internal/pkg/log/log.go
@@ -1,17 +1,20 @@
+// Copyright 2022 Innkeeper Belm(孔令飞) <nosbelm@qq.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/marmotedu/miniblog.
+
+// Package log is a log package used by miniblog project.
 package log
 
 import (
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 	"sync"
 	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-var (
-	mu  sync.Mutex
-	std = NewLogger(NewOptions())
-)
-
+// Logger 定义了 miniblog 项目的日志接口. 该接口只包含了支持的日志记录方法.
 type Logger interface {
 	Debugw(msg string, keysAndValues ...interface{})
 	Infow(msg string, keysAndValues ...interface{})
@@ -22,13 +25,22 @@ type Logger interface {
 	Sync()
 }
 
-var _ Logger = &zapLogger{}
-
+// zapLogger 是 Logger 接口的具体实现. 它底层封装了 zap.Logger.
 type zapLogger struct {
 	z *zap.Logger
 }
 
-// Init 指定选项初始化全局 logger
+// 确保 zapLogger 实现了 Logger 接口. 以下变量赋值，可以使错误在编译期被发现.
+var _ Logger = &zapLogger{}
+
+var (
+	mu sync.Mutex
+
+	// std 定义了默认的全局 Logger.
+	std = NewLogger(NewOptions())
+)
+
+// Init 使用指定的选项初始化 Logger.
 func Init(opts *Options) {
 	mu.Lock()
 	defer mu.Unlock()
@@ -36,42 +48,60 @@ func Init(opts *Options) {
 	std = NewLogger(opts)
 }
 
-// NewLogger 新建 logger
+// NewLogger 根据传入的 opts 创建 Logger.
 func NewLogger(opts *Options) *zapLogger {
 	if opts == nil {
 		opts = NewOptions()
 	}
 
+	// 将文本格式的日志级别，例如 info 转换为 zapcore.Level 类型以供后面使用
 	var zapLevel zapcore.Level
 	if err := zapLevel.UnmarshalText([]byte(opts.Level)); err != nil {
+		// 如果指定了非法的日志级别，则默认使用 info 级别
 		zapLevel = zapcore.InfoLevel
 	}
 
-	cfg := &zap.Config{
-		DisableCaller:     opts.DisableCaller,
-		DisableStacktrace: opts.DisableStacktrace,
-		Level:             zap.NewAtomicLevelAt(zapLevel),
-		Encoding:          opts.Format,
-		OutputPaths:       opts.OutputPaths,
-		EncoderConfig: zapcore.EncoderConfig{
-			MessageKey: "message",
-			LevelKey:   "level",
-			TimeKey:    "timestamp",
-			EncodeTime: func(time time.Time, encoder zapcore.PrimitiveArrayEncoder) {
-				encoder.AppendString(time.Format("2006-01-02 15:04:05.000"))
-			},
-			EncodeDuration: func(duration time.Duration, encoder zapcore.PrimitiveArrayEncoder) {
-				encoder.AppendFloat64(float64(duration) / float64(time.Millisecond))
-			},
-		},
+	// 创建一个默认的 encoder 配置
+	encoderConfig := zap.NewProductionEncoderConfig()
+	// 自定义 MessageKey 为 message，message 语义更明确
+	encoderConfig.MessageKey = "message"
+	// 自定义 TimeKey 为 timestamp，timestamp 语义更明确
+	encoderConfig.TimeKey = "timestamp"
+	// 指定时间序列化函数，将时间序列化为 `2006-01-02 15:04:05.000` 格式，更易读
+	encoderConfig.EncodeTime = func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString(t.Format("2006-01-02 15:04:05.000"))
 	}
-	z, err := cfg.Build()
+	// 指定 time.Duration 序列化函数，将 time.Duration 序列化为经过的毫秒数的浮点数
+	// 毫秒数比默认的秒数更精确
+	encoderConfig.EncodeDuration = func(d time.Duration, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendFloat64(float64(d) / float64(time.Millisecond))
+	}
+
+	// 创建构建 zap.Logger 需要的配置
+	cfg := &zap.Config{
+		// 是否在日志中显示调用日志所在的文件和行号，例如：`"caller":"miniblog/miniblog.go:75"`
+		DisableCaller: opts.DisableCaller,
+		// 是否禁止在 panic 及以上级别打印堆栈信息
+		DisableStacktrace: opts.DisableStacktrace,
+		// 指定日志级别
+		Level: zap.NewAtomicLevelAt(zapLevel),
+		// 指定日志显示格式，可选值：console, json
+		Encoding:      opts.Format,
+		EncoderConfig: encoderConfig,
+		// 指定日志输出位置
+		OutputPaths: opts.OutputPaths,
+		// 设置 zap 内部错误输出位置
+		ErrorOutputPaths: []string{"stderr"},
+	}
+
+	// 使用 cfg 创建 *zap.Logger 对象
+	z, err := cfg.Build(zap.AddStacktrace(zapcore.PanicLevel), zap.AddCallerSkip(1))
 	if err != nil {
 		panic(err)
 	}
 	logger := &zapLogger{z: z}
 
-	// 把标准库的 log.Logger 的 info 级别的输出重定向到 zap.logger
+	// 把标准库的 log.Logger 的 info 级别的输出重定向到 zap.Logger
 	zap.RedirectStdLog(z)
 
 	return logger
